5a5dae8a4e9da468cf165bea61d805630d26916d
Kaushika Weerakoon <91740359+kaushikaW@users.noreply.github.com>
2026-05-15 18:41:47 +0530

    runner script is updated

--- scripts/install_runner.sh
+++ scripts/install_runner.sh
@@ -3,27 +3,22 @@
 # This script downloads to VM and configures GitHub Actions Runner to run on the VM.
 # It takes a single argument, which is the token for the runner.
 
-set -e
-
-REPO_URL="https://github.com/kaushikaW/cocodoll"
 TOKEN="$1"
+REPO_URL="https://github.com/kaushikaW/cocodoll"
 
 apt update -y
 apt install -y curl git docker.io
 
-# allow runner user to use docker
-VM_USER=$(getent passwd 1000 | cut -d: -f1)
-usermod -aG docker $VM_USER
-
-# apply group change without reboot
-newgrp docker || true
-
 systemctl enable docker
 systemctl start docker
 
+usermod -aG docker azureadmin
 
-mkdir -p /actions-runner
-cd /actions-runner
+# run everything as azureadmin
+su - azureadmin <<EOF
+
+mkdir -p /home/azureadmin/actions-runner
+cd /home/azureadmin/actions-runner
 
 curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
 
@@ -37,3 +32,5 @@ tar xzf runner.tar.gz
 
 ./svc.sh install
 ./svc.sh start
+
+EOF
