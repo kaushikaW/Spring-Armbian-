31499e65efd66bdac27f383fbd976b7dda67bb33
Kaushika Weerakoon <91740359+kaushikaW@users.noreply.github.com>
2026-05-15 20:58:02 +0530

    runner script is updated

--- scripts/install_runner.sh
+++ scripts/install_runner.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
 TOKEN="$1"
-REPO_URL="https://github.com/kaushikaW/cocodoll"
+REPO="https://github.com/kaushikaW/cocodoll"
 
-# Update system
 sudo apt update -y
+sudo apt install -y curl git docker.io
 
-# install docker
-sudo apt update -y
-sudo apt install -y docker.io
-
-# Enable and start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Add the current user to the docker group to run Docker commands without sudo
 sudo usermod -aG docker azureadmin
 
-# Required dependencies
-sudo apt install -y curl tar git libicu-dev
-
-# Create user folder
 mkdir -p /home/azureadmin/actions-runner
 cd /home/azureadmin/actions-runner
 
-# Download Linux runner (IMPORTANT FIX)
-curl -o actions-runner.tar.gz -L \
+curl -o runner.tar.gz -L \
 https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
 
-# Extract
-tar xzf actions-runner.tar.gz
+tar xzf runner.tar.gz
 
-# Configure runner
 ./config.sh \
-  --url $REPO_URL \
+  --url $REPO \
   --token $TOKEN \
   --unattended \
   --replace
 
-# Install service
-sudo ./svc.sh install
-sudo ./svc.sh start
+./run.sh
