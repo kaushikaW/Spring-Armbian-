05fa9247df53bcdb57c597aeb8154dc594228e63
Kaushika Weerakoon <91740359+kaushikaW@users.noreply.github.com>
2026-05-15 23:05:19 +0530

    runner script is updated

--- scripts/install_runner.sh
+++ scripts/install_runner.sh
@@ -3,12 +3,26 @@
 TOKEN="$1"
 REPO="https://github.com/kaushikaW/cocodoll"
 
-sudo apt update -y
-sudo apt install -y curl git docker.io
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-sudo systemctl enable docker
-sudo systemctl start docker
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo apt update
 sudo usermod -aG docker azureadmin
 
 mkdir -p /home/azureadmin/actions-runner
@@ -18,11 +32,3 @@ curl -o runner.tar.gz -L \
 https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
 
 tar xzf runner.tar.gz
-
-./config.sh \
-  --url $REPO \
-  --token $TOKEN \
-  --unattended \
-  --replace
-
-./run.sh
