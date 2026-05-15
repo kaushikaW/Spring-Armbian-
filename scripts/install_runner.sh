0591057ee2d68acdabc7fa939c87f952c49b446a
Kaushika Weerakoon <91740359+kaushikaW@users.noreply.github.com>
2026-05-15 17:34:58 +0530

    git hub runner sh file add

---
+++ scripts/install_runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script downloads to VM and configures GitHub Actions Runner to run on the VM.
+# It takes a single argument, which is the token for the runner.
+
+set -e
+
+REPO_URL="https://github.com/kaushikaW/cocodoll"
+TOKEN="$1"
+
+apt update -y
+apt install -y curl git docker.io
+
+systemctl enable docker
+systemctl start docker
+
+mkdir -p /actions-runner
+cd /actions-runner
+
+curl -o runner.tar.gz -L https://github.com/actions/runner/releases/download/v2.334.0/actions-runner-linux-x64-2.334.0.tar.gz
+
+tar xzf runner.tar.gz
+
+./config.sh \
+  --url $REPO_URL \
+  --token $TOKEN \
+  --unattended \
+  --replace
+
+./svc.sh install
+./svc.sh start
